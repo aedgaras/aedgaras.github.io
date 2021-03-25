416a41d518e6522d81f518e8d9fe479bb5b77977
aedgaras <66108869+aedgaras@users.noreply.github.com>
2021-03-25 08:26:53 +0200

    added links

---
+++ index.js
@@ -0,0 +1,9 @@
+function imageEnlarge(x) {
+    x.classList.remove("fa-2x");
+    x.classList.add("fa-3x");
+}
+
+function imageNormalize(x) {
+    x.classList.remove("fa-3x");
+    x.classList.add("fa-2x");
+}
